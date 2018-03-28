561f82270b20a47f3019cbdd0803f6389176d325
Zack Yang <zack9433@gmail.com>
2018-03-28 18:07:26 +0800

    modify

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:8.9.4
 
 LABEL author="Zack Yang <zack9433@gmail.com>"
 
-RUN npm install -g conventional-changelog-angular conventional-recommended-bump
+RUN npm install -g semver conventional-changelog-angular conventional-recommended-bump
 
 ADD clog /bin/
 ADD drone-clog.sh /bin/
--- drone-clog.sh
+++ drone-clog.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+git config --global user.email "isd@moxa.com"
+git config --global user.name "isd.droneci"
+git config --global push.default current
+
 BUMP=$(conventional-recommended-bump -p angular)
 
+LATEST_TAG=$(git describe --abbrev=0 --tags)
+
+NEXT_TAG=$(semver -i "$BUMP" "$LATEST_TAG")
+
 clog -F --"$BUMP" -C ${PLUGIN_CHANGELOG_FILE}
+
+git tag -a "NEXT_TAG" -m "chore(release): bump up version! :rocket: [SKIP CI]"
+
+git push --no-verify --follow-tags
 
